d7a4ca2fa11fee3b5abc58bc7322ce5c28d5a4ea
Akash Jain <akashjain0708@gmail.com>
2015-12-05 23:36:03 -0600

    Sign Up started

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
 from flask import Flask, render_template
+from flask.ext.pymongo import PyMongo
+from pymongo import MongoClient
+
+def connect():
+# Substitute the 5 pieces of information you got when creating
+# the Mongo DB Database (underlined in red in the screenshots)
+# Obviously, do not store your password as plaintext in practice
+    connection = MongoClient("ds049651.mongolab.com",49651)
+    handle = connection["scheduler"]
+    handle.authenticate("akash22jain","Urgointobemad1")
+    return handle
+
 app = Flask(__name__)
+mongo = PyMongo(app)
+handle = connect()
 
 @app.route("/")
 def main():
 	return render_template('index.html');
+
+@app.route("/signUp")
+def signUp():
+	return render_template("signUp.html");
 
 if __name__ == '__main__':
 	app.run()
